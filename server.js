d6a5d29dd0729e50eec3f2707c329a388b1487bc
Yanians <brian25198928@gmail.com>
2022-02-02 19:00:13 +0800

    another commit

--- server.js
+++ server.js
@@ -21,39 +21,3 @@ const publicPath = path.join(__dirname,'.','dist');
       app.listen(PORT,()=> {
       	console.log(`Server is running on port ${PORT}. `);
       });
-
-      
-// app.set('port',(process.env.PORT || 4000));
-
-
-// const publicPath = path.join(__dirname,'.','public');
-
-// const port = process.env.PORT || 9000;
-
-//       app.use(express.static(publicPath));
-
-//       app.get('/*', function(req, res){
-//             res.sendFile(path.join(publicPath,'index.html'));
-//       });
-
-//       app.listen(port,()=> {
-//         console.log(`Server is running on port ${port}. `);
-//       });
-
-// var express = require('express');
-// var cons = require('consolidate');
-// var app = express();
-
-// app.engine('html',cons.handlebars);
-// app.set('port',(process.env.PORT || 4000));
-// app.set('view engine', 'html');
-// app.set('views', __dirname + '/public');
-// app.use(express.static('public'));
-
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join('index'));
-// });
- 
-// app.listen(app.get('port'),function(){
-//   console.log("Server is running at localhost:4000");
-// });
