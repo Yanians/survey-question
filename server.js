892802d8bd1ce06680a595262400f4c002290e00
Yanians <brian25198928@gmail.com>
2022-02-02 19:35:14 +0800

    update onhand

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 
 
 const express = require('express'),
-   jsonserver = require('json-server'),
-       server = jsonserver.create(),
-       router = jsonserver.router('db.json'),
-   middleware = jsonserver.defaults(),
+   // jsonserver = require('json-server'),
+   //     server = jsonserver.create(),
+   //     router = jsonserver.router('db.json'),
+   // middleware = jsonserver.defaults(),
          path = require('path'),
           app = express();
 
 const publicPath = path.join(__dirname,'.','dist');
       app.use(express.static(publicPath));
-      app.use(middleware);
-      app.use(router);
+      // app.use(middleware);
+      // app.use(router);
       app.get('/*', function(req, res){
             res.sendFile(path.join(publicPath,'index.html'))
       });      
