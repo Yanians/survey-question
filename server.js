2cc36146c7aed9f400b75bdf333fe4c275eb77f8
Yanians <brian25198928@gmail.com>
2022-02-03 13:53:55 +0800

    updated

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 
 
-const express = require('express'),
-   jsonserver = require('json-server'),
+// const express = require('express'),
+   const jsonserver = require('json-server'),
          path = require('path'),
        server = jsonserver.create(),
        router = jsonserver.router('./db.json'),
-   middleware = jsonserver.defaults({static:"./public"}),
-          app = express(),
+   middleware = jsonserver.defaults({static:"./public"});
+          // app = express(),
 
-   publicPath = path.join(__dirname,'.','public');
+   // publicPath = path.join(__dirname,'.','public');
+   //    server.use(middleware);
+   //    server.use(router);
+   //       app.use(express.static(publicPath));
+   //       app.use(server);
+   //       app.get('/*', function(req, res){
+   //          res.sendFile(path.join(publicPath,'index.html'))
+   //    });      
+      //set port, listen for requests
+ const PORT = process.env.PORT || 9000;
       server.use(middleware);
       server.use(router);
-         app.use(express.static(publicPath));
-         app.use(server);
-         app.get('/*', function(req, res){
-            res.sendFile(path.join(publicPath,'index.html'))
-      });      
-      //set port, listen for requests
-      const PORT = process.env.PORT || 9000;
       server.listen(PORT,()=> {
       	console.log(`Server is running on port ${PORT}. `);
       });
