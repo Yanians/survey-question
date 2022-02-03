a77acfc012e497283e562263833448b578e1843b
Yanians <brian25198928@gmail.com>
2022-02-03 11:11:00 +0800

    now updated

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const express = require('express'),
             res.sendFile(path.join(publicPath,'index.html'))
       });      
       //set port, listen for requests
-      const PORT = process.env.port || 9000;
+      const PORT = process.env.PORT || 9000;
       app.listen(PORT,()=> {
       	console.log(`Server is running on port ${PORT}. `);
       });
