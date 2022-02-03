5531d79ac00ca953f087fc04999f4b1c62d958fd
Yanians <brian25198928@gmail.com>
2022-02-03 12:18:17 +0800

    updated 1s ago

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ const express = require('express'),
       });      
       //set port, listen for requests
       const PORT = process.env.PORT || 9000;
-      app.listen(PORT,()=> {
+      server.listen(PORT,()=> {
       	console.log(`Server is running on port ${PORT}. `);
       });
